8be67547cc722fe3c09f88258d551aa6a9cb57b3
Andres Mendez <andrex873@gmail.com>
2013-05-03 19:41:21 -0500

    Se agrega modificaciones clase 01 taller 2

---
+++ Clase01/BankPrj/src/Account.java
@@ -0,0 +1,38 @@
+/*
+ * Account.java
+ *
+ * Created on November 9, 2005, 12:46 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+public class Account {
+
+    private double balance;
+
+    /**
+     * Creates a new instance of Account
+     */
+    public Account(double initBalance) {
+        balance = initBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amt) {
+        this.balance = this.balance + amt;
+    }
+
+    public void withdraw(double amt) {
+        if (balance >= amt) {
+            balance = balance - amt;
+        }
+    }
+}
